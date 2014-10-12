2470cec38b7cdcf70fe1f0e635f01f49bd4ad82f
flsf <jianmaflsf@gmail.com>
2014-10-12 21:12:04 +0800

    update clean_info

--- clean_info.py
+++ clean_info.py
@@ -30,11 +30,13 @@ shortname  := {{ shortname }}
 
 MYNAME = u''
 SHORTNAME = u''
-TOOLS = u'sqlmap'
-TOOLSDESC = u'SQL注入测试工具'
+TOOLS = u'Firefox'
+TOOLSDESC = u'浏览器'
 VULDATE = u'2014-09-'
 
-info_words = {'vuldate': '', 'vuleffect': '', 'vultype': '', 'vulvendor': '', 'vuldesc': '', 'vulreferer': '', 'tools': TOOLS, 'toolsdesc': TOOLSDESC, 'myname': MYNAME, 'shortname': SHORTNAME}
+SITE = ''
+
+info_words = {'vuldate': VULDATE, 'vuleffect': '', 'vultype': '', 'vulvendor': '', 'vuldesc': '', 'vulreferer': '', 'tools': TOOLS, 'toolsdesc': TOOLSDESC, 'myname': MYNAME, 'shortname': SHORTNAME}
 
 def extract_vars(template):
     keys = set()
@@ -57,28 +59,41 @@ def read_info_content(url):
     print '[*] read info from ' + url
     content = urllib.urlopen(url).read()
     soup = BeautifulSoup(content)
-    info_list = soup.find("div", class_="content").find_all("h3")
+    print SITE
+    if SITE == 'wooyun':
+        info_list = soup.find("div", class_="content").find_all("h3")
+    elif SITE == 'exp-db':
+        pass
     return info_list
 
 def read_vultype(info):
     vultype = u''
-    title_info = info[5].string
-    for sql_key in sql_list:
-        if sql_key in title_info:
-            vultype = sql_list[0]
+    if SITE == 'wooyun':
+        title_info = info[5].string
+        for sql_key in sql_list:
+            if sql_key in title_info:
+                vultype = sql_list[0]
+    elif SITE == 'exp-db':
+        pass
     return vultype
 
 def read_vulvendor(info):
-    vendor_info = info[2].a.get("href").encode("utf-8")
-    content = urllib.urlopen(vendor_info).read()
-    soup = BeautifulSoup(content)
-    url_info = soup.find("div", class_="content").h3.string
-    vulvendor = re.search('http.*', url_info).group(0)
+    if SITE == 'wooyun':
+        vendor_info = info[2].a.get("href").encode("utf-8")
+        content = urllib.urlopen(vendor_info).read()
+        soup = BeautifulSoup(content)
+        url_info = soup.find("div", class_="content").h3.string
+        vulvendor = re.search('http.*', url_info).group(0)
+    elif SITE == 'exp-db':
+        pass
     return vulvendor
 
 def read_vuldate(info):
-    date_info = info[4].string
-    vuldate = re.search('20[1-2][3-9]-[0-9]+-[0-9]+', date_info).group(0)
+    if SITE == 'wooyun':
+        date_info = info[4].string
+        vuldate = re.search('20[1-2][3-9]-[0-9]+-[0-9]+', date_info).group(0)
+    elif SITE == 'exp-db':
+        pass
     return vuldate
 
 def read_vuleffect(vultype):
@@ -86,28 +101,43 @@ def read_vuleffect(vultype):
         vuleffect = u'SQL注入,泄露信息'
     return vuleffect
 
-def main():
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        info = read_info_content(url)
-        vultype = read_vultype(info)
-        vulvendor = read_vulvendor(info)
-        print vulvendor
-        vuldate = read_vuldate(info)
-        vuleffect = read_vuleffect(vultype)
-        vulreferer = url
-        info_words['vultype'] = vultype
-        info_words['vulvendor'] = vulvendor
-        info_words['vuldate'] = vuldate
-        info_words['vulreferer'] = url
-        info_words['vuleffect'] = vuleffect
-    
+def check_site(url):
+    global SITE
+    if 'wooyun' in url:
+        SITE = 'wooyun'
+        read_from_wooyun(url)
+    elif 'exploit-db' in url:
+        SITE = 'exp-db'
+        print 'exp-db'
+
+def read_from_wooyun(url):
+    info = read_info_content(url)
+    vultype = read_vultype(info)
+    vulvendor = read_vulvendor(info)
+    vuldate = read_vuldate(info)
+    vuleffect = read_vuleffect(vultype)
+    vulreferer = url
+    info_words['vultype'] = vultype
+    info_words['vulvendor'] = vulvendor
+    info_words['vuldate'] = vuldate
+    info_words['vulreferer'] = url
+    info_words['vuleffect'] = vuleffect
+
+def clean_info():
     f = open('poc_info.txt', 'w')
     info = generate_info(info_temp, info_words)
     print info
     f.write(info.encode('utf-8'))
     f.close()
-    print '[+] poc_info have cleared.'
+    print '[+] poc_info cleared.'
+
+
+def main():
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        check_site(url)
+
+    clean_info()
 
 if __name__ == "__main__":
     main()
