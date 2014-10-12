bf7cf314c537d62d40eb3f1929384638cb220f06
flsf <jianmaflsf@gmail.com>
2014-10-12 19:06:34 +0800

    add report_maker

---
+++ report_maker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from weekdays import weekdays
+
+MYNAME = '[flsf]'
+
+def make_report():
+    dirname = weekdays()
+    report_name = '[周报]' + MYNAME + dirname.replace(':', '/')
+    pocs = []
+    if os.path.exists(dirname):
+        pocs = os.listdir(dirname)
+
+    week_job = ['\n本周工作\n']
+    for index, pocname in enumerate(pocs):
+        week_job.append(str(index + 1) + '.' + pocname + ' POC+文档  [已经完成]')
+
+    week_job.append('\n\n下周工作\n继续wsl工作')
+    week_job.append('\n\n唧唧歪歪')
+
+    report = report_name + '\n'
+    for _ in week_job:
+        report += _
+
+    print report
+
+    report_file = open(report_name.replace('/', ':') + '.txt', 'w')
+    report_file.write(report)
+
+def main():
+    make_report()
+
+if __name__ == '__main__':
+    main()
