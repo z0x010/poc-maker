b63f2346d23d6c1026e9556c49e918d1679d443a
flsf <jianmaflsf@gmail.com>
2014-10-30 23:37:01 +0800

    fix modify_template lost

---
+++ utils/modify_template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def modify_poc_template(dict):
+    if dict['info_post_data']:
+        dict['info_post_data'] = u'payload = \'{data}\'\n        response = req.post(self.url + target_url, data=payload, timeout=10)'.format(data=dict['info_post_data'])
+    else:
+        dict['info_post_data'] = u'\n        response = req.get(self.url + target_url, timeout=10)'
+    if dict['info_match']:
+        dict['info_match'] = u'\n        match = re.search(\'{match}\', content)'.format(match=dict['info_match'])
+    if dict['info_other_match']:
+        dict['info_other_match'] = u'match_other = re.search(\'{match}\', content)\n\n        if match and match_other:'.format(match=dict['info_other_match'])
+    else:
+        dict['info_other_match'] = u'\n        if match:'
+
+
